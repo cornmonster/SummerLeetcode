3defe41f6d139acea96a6dc63cb6a8b170ee70f8
cornmonster <zhongwang.dt@gmail.com>
2017-06-11 18:40:51 -0400

    nothing

--- 101. Symmetric Tree.py
+++ 101. Symmetric Tree.py
@@ -6,9 +6,23 @@
 #         self.right = None
 
 class Solution(object):
+    def helper(self, p, q):
+        if(p == None and q == None):
+            return True
+        elif(p == None and q != None or p != None and q == None):
+            return False
+        elif(p.val != q.val):
+            return False
+        else:
+            return self.helper(p.left, q.right) and self.helper(p.right, q.left)
+
     def isSymmetric(self, root):
         """
         :type root: TreeNode
         :rtype: bool
         """
+        # my solution
+        if(root == None):
+            return True
+        return self.helper(root.left, root.right)
         
